4852427906c80fc3b835a90aaef488309daf856e
Marc Delagrammatikas <delagrammatikas@gmail.com>
2017-01-13 07:06:09 -0800

    Adds shopping sub-app with a session module to handle user sessions.

---
+++ apps/shopping/lib/shopping.ex
@@ -0,0 +1,2 @@
+defmodule ElixirStore.Shopping do
+end
---
+++ apps/shopping/test/shopping_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirStore.ShoppingTest do
+  use ExUnit.Case
+  doctest ElixirStore.Shopping
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
