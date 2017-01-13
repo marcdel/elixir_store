8795e29d938ec5a52bedd4371cd4a2eb99c3b68b
Marc Delagrammatikas <mdelagrammatikas@truecar.com>
2017-01-13 11:34:39 -0800

    Add calls to select, remove, and change items in the session.

--- apps/shopping/lib/shopping/session.ex
+++ apps/shopping/lib/shopping/session.ex
@@ -1,8 +1,22 @@
 defmodule ElixirStore.Shopping.Session do
   use GenServer
 
-  def start_link(%{key: key} = args) do
-    GenServer.start_link(__MODULE__, args, name: key)
+  def start_link(%{key: key} = args), do: GenServer.start_link(__MODULE__, args, name: key)
+  def init(%{key: key} = args), do: struct(%ElixirStore.Shopping.Session{}, args)
+
+  def select_item(pid, item), do: GenServer.call(pid, {:select_item, item})
+  def remove_item(pid, sku: sku), do: GenServer.call(pid, {:remove_item, sku: sku})
+  def change_item(pid, sku: sku), do: GenServer.call(pid, {:change_item, sku: sku})
+
+  def handle_call({:select_item, item}, _sender, session) do
+    session = %{session | items: List.insert_at(session.items, -1, item)}
+    {:reply, session, session}
+  end
+
+  def handle_call({:remove_item, sku: sku}, _sender, session) do
+  end
+
+  def handle_call({:change_item, sku: sku}, _sender, session) do
   end
 
   defstruct [
