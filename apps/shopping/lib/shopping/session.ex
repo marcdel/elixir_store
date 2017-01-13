e826bb3065362a1f837a0a38bd49d3d1b28b35ed
Marc Delagrammatikas <mdelagrammatikas@truecar.com>
2017-01-13 11:53:13 -0800

    Updates session to write to database when and item is selected.

--- apps/shopping/lib/shopping/session.ex
+++ apps/shopping/lib/shopping/session.ex
@@ -1,23 +1,6 @@
 defmodule ElixirStore.Shopping.Session do
   use GenServer
-
-  def start_link(%{key: key} = args), do: GenServer.start_link(__MODULE__, args, name: key)
-  def init(%{key: key} = args), do: struct(%ElixirStore.Shopping.Session{}, args)
-
-  def select_item(pid, item), do: GenServer.call(pid, {:select_item, item})
-  def remove_item(pid, sku: sku), do: GenServer.call(pid, {:remove_item, sku: sku})
-  def change_item(pid, sku: sku), do: GenServer.call(pid, {:change_item, sku: sku})
-
-  def handle_call({:select_item, item}, _sender, session) do
-    session = %{session | items: List.insert_at(session.items, -1, item)}
-    {:reply, session, session}
-  end
-
-  def handle_call({:remove_item, sku: sku}, _sender, session) do
-  end
-
-  def handle_call({:change_item, sku: sku}, _sender, session) do
-  end
+  alias ElixirStore.Shopping.Db
 
   defstruct [
     domain: nil,
@@ -30,4 +13,30 @@ defmodule ElixirStore.Shopping.Session do
     logs: [],
     discounts: []
   ]
+
+  def start_link(%{key: key} = args), do: GenServer.start_link(__MODULE__, args, name: key)
+
+  def init(%{key: key} = args) do
+    session = Db.find_or_create_session(args)
+    {:ok, session}
+  end
+
+  def select_item(pid, item), do: GenServer.call(pid, {:select_item, item})
+  def remove_item(pid, sku: sku), do: GenServer.call(pid, {:remove_item, sku: sku})
+  def change_item(pid, sku: sku), do: GenServer.call(pid, {:change_item, sku: sku})
+
+  def handle_call({:select_item, item}, _sender, session) do
+    session = %{session | items: List.insert_at(session.items, -1, item)} |> save_session
+    {:reply, session, session}
+  end
+
+  def handle_call({:remove_item, sku: sku}, _sender, session) do
+  end
+
+  def handle_call({:change_item, sku: sku}, _sender, session) do
+  end
+
+  def save_session(%ElixirStore.Shopping.Session{} = session, log: log) do
+    %{session | logs: List.insert_at(session.logs, -1, %{entry: log})} |> Db.save_session
+  end
 end
